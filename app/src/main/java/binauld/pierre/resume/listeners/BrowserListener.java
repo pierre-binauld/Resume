33642fdead3d1ac95081b29fd4d66343daedc5fe
Pierre Binauld <pierre.binauld@gmail.com>
2015-10-30 13:49:38 +0100

    Management of error in listener.

--- app/src/main/java/binauld/pierre/resume/listeners/BrowserListener.java
+++ app/src/main/java/binauld/pierre/resume/listeners/BrowserListener.java
@@ -5,7 +5,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import binauld.pierre.resume.R;
+
+/**
+ * A listener listening onClick event and send an intent to start the web browser.
+ * The view sender of the event must have a string url as tag.
+ */
 public class BrowserListener implements ImageButton.OnClickListener {
 
     private Context context;
@@ -17,10 +24,14 @@ public class BrowserListener implements ImageButton.OnClickListener {
     @Override
     public void onClick(View v) {
         String query = (String) v.getTag();
-        Uri webpage = Uri.parse(query);
-        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-        if (intent.resolveActivity(context.getPackageManager()) != null) {
-            context.startActivity(intent);
+        if (null != query) {
+            Uri webPage = Uri.parse(query);
+            Intent intent = new Intent(Intent.ACTION_VIEW, webPage);
+            if (intent.resolveActivity(context.getPackageManager()) != null) {
+                context.startActivity(intent);
+            }
+        } else {
+            Toast.makeText(context, R.string.message_error_no_web_page, Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/binauld/pierre/resume/listeners/LocationListener.java
+++ app/src/main/java/binauld/pierre/resume/listeners/LocationListener.java
@@ -5,7 +5,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import binauld.pierre.resume.R;
+
+/**
+ * A listener listening onClick event and send an intent to start the a geo localisation application.
+ * The view sender of the event must have a string location as tag.
+ */
 public class LocationListener implements ImageButton.OnClickListener {
 
     private Context context;
@@ -17,11 +24,15 @@ public class LocationListener implements ImageButton.OnClickListener {
     @Override
     public void onClick(View v) {
         String query = (String) v.getTag();
-        Uri geo = Uri.parse("geo:0,0?q=" + query);
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(geo);
-        if (intent.resolveActivity(context.getPackageManager()) != null) {
-            context.startActivity(intent);
+        if (null != query) {
+            Uri geo = Uri.parse("geo:0,0?q=" + query);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(geo);
+            if (intent.resolveActivity(context.getPackageManager()) != null) {
+                context.startActivity(intent);
+            }
+        } else {
+            Toast.makeText(context, R.string.message_error_no_location, Toast.LENGTH_SHORT).show();
         }
     }
 }
